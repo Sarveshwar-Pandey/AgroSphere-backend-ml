67459db5dea05b8d30baf53989da61147f2a2d85
Sarveshwar <sarveshwar2810@gmail.com>
2026-04-27 16:30:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -226,8 +226,4 @@ def disease_prediction():
 # ===============================================================================================
 
 if __name__ == '__main__':
-    app.run(debug=False)
-
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5000)
-
+    app.run(host="0.0.0.0", port=5000)
